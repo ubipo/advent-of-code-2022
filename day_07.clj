7c32f05e9864a4d4c7be6d5104496e608f0896c4
Pieter Fiers <pieter@pfiers.net>
2022-12-07 19:23:24 +0100

    Add day 7

---
+++ day_07.clj
@@ -0,0 +1,63 @@
+(ns day-07
+  (:require [clojure.string :as str]
+            [input :refer [load-day-input]]
+            [split :refer [split-by-inclusive]]))
+
+
+(def shell-prompt "$ ")
+
+(defn transcript->commands [transcript]
+  (->> transcript
+       str/split-lines
+       (split-by-inclusive #(str/starts-with? % shell-prompt))))
+
+(defn parse-ls-line [line]
+  (let [[size-str name] (str/split line #"\s+")]
+    [name (if (= size-str "dir") {} (parse-long size-str))]))
+
+(defn commands->tree [commands]
+  (first
+   (reduce
+    (fn [[root-dir cwd] [command-line & output-lines]]
+      (let [[command args] (str/split (subs command-line (count shell-prompt)) #"\s+" 2)]
+        (case command
+          "cd" [root-dir (case args
+                           "/" []
+                           ".." (if (empty? cwd) [] (pop cwd))
+                           (conj cwd args))]
+          "ls" (let [children (into {} (map parse-ls-line output-lines))
+                     new-root-dir (if (empty? cwd) children (assoc-in root-dir cwd children))]
+                 [new-root-dir cwd]))))
+    [{} []]
+    commands)))
+
+(defn load-input []
+  (->> (load-day-input 7)
+       transcript->commands
+       commands->tree))
+
+(defn dir->sizes [dir]
+  (reduce
+   (fn [[size children] [_name size-or-subdir]]
+     (if (map? size-or-subdir)
+       (let [[subdir-size subdir-children] (dir->sizes size-or-subdir)]
+         [(+ size subdir-size) (apply conj children subdir-size subdir-children)])
+       [(+ size size-or-subdir) children]))
+   [0 []]
+   dir))
+
+(defn part-one [tree]
+  (apply + (filter #(<= % 100000) (flatten (dir->sizes tree)))))
+
+(def fs-size 70000000)
+(def update-size 30000000)
+
+(defn part-two [input]
+  (let [dir-sizes (dir->sizes input)
+        size-to-clear (- update-size (- fs-size (first dir-sizes)))]
+    (first (sort (filter #(>= % size-to-clear) (flatten dir-sizes))))))
+
+(defn -main [& _args]
+  (let [input (load-input)]
+    (println "Part one:" (part-one input))
+    (println "Part two:" (part-two input))))
--- split.clj
+++ split.clj
@@ -3,8 +3,7 @@
   
 (defn split-by
   "Returns a vector of vectors for each group in coll seperated by elements for 
-   which pred returns logical true (excluding the element that satisfies the 
-   predicate itself, unlike clojure.core/split-with)"
+   which pred returns logical true, excluding the seperator element itself"
   [pred coll]
   (reduce
    (fn [acc x]
@@ -12,6 +11,18 @@
        (if (seq (peek acc)) (conj acc []) acc)
        (conj (pop acc) (conj (peek acc) x))))
    [[]] coll))
+
+(defn split-by-inclusive
+  "Returns a vector of vectors for each group in coll seperated by elements for 
+   which pred returns logical true, including the seperator element in the next
+   group"
+  [pred coll]
+  (reduce
+   (fn [acc x]
+     (if (pred x)
+       (conj acc [x])
+       (conj (pop acc) (conj (peek acc) x))))
+   [] coll))
 
 (defn split-on
   "Returns a vector of vectors for each group in coll seperated by element (
