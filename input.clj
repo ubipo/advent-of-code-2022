8dcb2a3b608fd9e1416b425d14f7ac1bd57268a6
Pieter Fiers <pieter@pfiers.net>
2022-12-28 20:54:57 +0100

    Clean load-day-input

--- input.clj
+++ input.clj
@@ -2,17 +2,19 @@
   (:require [clj-http.client :as http]
             [clojure.java.io :refer [make-parents]]))
 
+
+(defn load-uncached-day-input [day] 
+  (:body
+   (#(http/get
+      (str "https://adventofcode.com/2022/day/" day "/input")
+      {:headers {:Cookie (str "session=" (slurp "session_cookie"))}}))))
+
 (defn load-day-input [day]
   (let [day-cache-filename (format "input/day_%02d.txt" day)]
     (make-parents day-cache-filename)
     (try
       (slurp day-cache-filename)
       (catch java.io.FileNotFoundException _
-        (->> (slurp "session_cookie")
-            (#(http/get
-               (str "https://adventofcode.com/2022/day/" day "/input")
-               {:headers {:Cookie (str "session=" %)}}))
-            :body
-            (#(let [day-input %] 
-                (spit day-cache-filename day-input) 
-                day-input)))))))
+        (let [day-input (load-uncached-day-input day)]
+          (spit day-cache-filename day-input)
+          day-input)))))
